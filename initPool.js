907f08a4db01799b8ab7b0f030eace9f35ad7d26
Liam Leung <liam.leung@binery.co>
2022-04-22 11:18:58 +0800

    feat: add heroku dg

--- initPool.js
+++ initPool.js
@@ -1,12 +1,26 @@
 import pg from "pg";
 
 const { Pool } = pg;
-const pgConnectionConfigs = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-};
+
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
+  pgConnectionConfigs = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  };
+}
 
 const pool = new Pool(pgConnectionConfigs);
 
